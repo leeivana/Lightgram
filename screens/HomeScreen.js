d9fa74f9acdacff2065167d4622b3d95530dabcc
leeivana <lee.ivana@hotmail.com>
2018-12-16 14:40:20 -0500

    Changed file names

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,49 +1,48 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView, Button } from 'react-native';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Button,
+} from 'react-native';
 import { WebBrowser } from 'expo';
-import Jumbotron from '../components/Jumbotron';
-import PhoneInput from '../components/PhoneInput';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = ({ navigation, screenProps }) => ({
-    headerStyle: { borderBottomWidth: 0 },
+    title: null,
     headerRight: <Button title="Next" onPress={(navigation)=>{ navigation.navigate('DrawerOpen'); }} />,
     headerLeft: <Button title="Back" onPress={(navigation)=>{ navigation.navigate('DrawerOpen'); }} />,
   });
 
-  //call setstate for phone number
-  constructor(props) {
-    super(props);
-    this.state = {
-      phoneNumber: '',
-    };
-  }
-
-  _handlePress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
-
   render() {
-    const { container, topContainer, middleContainer } = styles;
-
     return (
-      <ScrollView
-        style={container}
-        keyboardDismissMode="on-drag"
-        keyboardShouldPersistTaps="handled"
-      >
-        <View style={topContainer}>
-          <Jumbotron
-            mainText='Your Phone'
-            subtext='Please confirm your country code and enter your phone number'
-          />
-        </View>
-        <View style={middleContainer}>
-          <PhoneInput />
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require('../assets/images/robot-dev.png')
+                  : require('../assets/images/robot-prod.png')
+              }
+              style={styles.welcomeImage}
+            />
+          </View>
+          <View style={styles.helpContainer}>
+            <TouchableOpacity
+              onPress={this._handleHelpPress}
+              style={styles.helpLink}
+            />
+          </View>
+        </ScrollView>
+      </View>
     );
   }
 }
@@ -53,16 +52,86 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  topContainer: {
-    paddingTop: 150,
-    paddingBottom: 0,
-    flex: 2,
-    justifyContent: 'center',
-    alignSelf: 'center',
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
   },
-  middleContainer: {
-    flex: 3,
-    justifyContent: 'flex-start',
-    alignSelf: 'stretch',
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
   },
 });
