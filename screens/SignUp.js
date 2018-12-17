eef5845b9bb178a2b64fe5a6a15f033712f09365
leeivana <lee.ivana@hotmail.com>
2018-12-17 03:42:24 -0500

    Styling for login page

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,15 +1,39 @@
 import React, { Fragment } from 'react';
-import { View, Button, TextInput, StyleSheet } from 'react-native';
-
+import { View, Button, TextInput, StyleSheet, Text } from 'react-native';
 import { Auth } from 'aws-amplify';
+import Jumbotron from '../components/Jumbotron';
 
 const initialState = {
-  username: '',
+  username: '+1',
   authenticationCode: '',
   showConfirmationForm: false,
 };
 
 export default class SignUp extends React.Component {
+  // static navigationOptions = ({ navigation, screenProps }) => ({
+  //   title: 'SignUp',
+  //   headerStyle: { borderBottomWidth: 0, color: black },
+  //   headerRight: (
+  //     <Button
+  //       title="Next"
+  //       onPress={navigation => {
+  //         navigation.navigate('DrawerOpen');
+  //       }}
+  //     />
+  //   ),
+  //   headerLeft: (
+  //     <Button
+  //       title="Back"
+  //       onPress={navigation => {
+  //         navigation.navigate('DrawerOpen');
+  //       }}
+  //     />
+  //   ),
+  // });
+  // static navigationOptions = {
+  //   title: 'Home',
+  // };
+
   state = initialState;
 
   onChangeText = (key, val) => {
@@ -44,50 +68,71 @@ export default class SignUp extends React.Component {
   };
 
   render() {
+    const { topContainer, middleContainer, phoneInput } = styles;
+    const { showConfirmationForm } = this.state;
     return (
-      <View style={styles.container}>
-        {!this.state.showConfirmationForm && (
-          <Fragment>
-            <TextInput
-              style={styles.input}
-              placeholder="Username"
-              autoCapitalize="none"
-              placeholderTextColor="white"
-              onChangeText={val => this.onChangeText('username', val)}
-            />
-            <Button title="Sign Up" onPress={this.signUp} />
-          </Fragment>
+      <View>
+        {!showConfirmationForm && (
+          <View>
+            <View style={topContainer}>
+              <Jumbotron
+              mainText="Your Phone"
+              subtext="Please confirm your country code and enter your phone number"
+              />
+            </View>
+            <View style={middleContainer}>
+              <TextInput
+                value={this.state.username}
+                keyboardType="phone-pad"
+                style={phoneInput}
+                placeholder="Your Phone Number"
+                onChangeText={val => this.onChangeText('username', val)}
+              />
+              <Button title="Sign Up" onPress={this.signUp} />
+            </View>
+          </View>
         )}
-        {this.state.showConfirmationForm && (
-          <Fragment>
-            <TextInput
-              style={styles.input}
-              placeholder="Authentication code"
-              autoCapitalize="none"
-              placeholderTextColor="white"
-              onChangeText={val => this.onChangeText('authenticationCode', val)}
-            />
-            <Button title="Confirm Sign Up" onPress={this.confirmSignUp} />
-          </Fragment>
+        {showConfirmationForm && (
+          <View>
+            <View style={topContainer}>
+              <Jumbotron
+                mainText="Authentication"
+                subtext="Please enter your Authorization Code"
+              />
+            </View>
+            <View style={middleContainer}>
+              <TextInput
+                style={phoneInput}
+                keyboardType="phone-pad"
+                placeholder="Authentication Code"
+                onChangeText={val =>
+                  this.onChangeText('authenticationCode', val)
+                }
+              />
+              <Button title="Confirm Sign Up" onPress={this.confirmSignUp} />
+            </View>
+          </View>
         )}
       </View>
     );
   }
 }
-
+const offset = 24;
 const styles = StyleSheet.create({
-  input: {
-    width: 350,
-    height: 55,
-    backgroundColor: '#42A5F5',
-    margin: 10,
-    padding: 8,
-    color: 'white',
-    borderRadius: 14,
+  topContainer: {
+    marginTop: 150,
+    marginBottom: 0,
   },
-  container: {
-    marginTop: 300,
-    flex: 1,
-    justifyContent: 'center',
+  middleContainer: {
+    justifyContent: 'flex-start',
+    alignSelf: 'stretch',
+  },
+  phoneInput: {
+    height: offset * 2.5,
+    margin: offset,
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.3,
+    fontSize: 23,
+    textAlign: 'justify',
   },
 });
