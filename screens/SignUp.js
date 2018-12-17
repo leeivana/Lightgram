05bee0db890911794802e5c7c100d92b72feed07
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-12-17 16:44:54 -0500

    changed font size

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -155,7 +155,7 @@ export default class SignUp extends React.Component {
                   this.phone = ref;
                 }}
                 style={phoneInput}
-                textStyle={{ fontSize: 30 }}
+                textStyle={{ fontSize: 21 }}
                 flagStyle={{ height: 25, width: 45 }}
                 value={username}
                 placeholder="Your Phone Number"
