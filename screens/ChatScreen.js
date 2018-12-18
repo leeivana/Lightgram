4dd64cbd822bc6dde73fc9d286d054cd8298c2ed
leeivana <lee.ivana@hotmail.com>
2018-12-17 23:03:53 -0500

    Added code to render chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
-
-const styles = StyleSheet.create({
-  textStyle: {
-    paddingTop: 100,
-    fontSize: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+import {
+  GiftedChat,
+  Actions,
+  Bubble,
+  SystemMessage,
+} from 'react-native-gifted-chat';
 
 class ChatScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
-    title: (navigation.state.params || {}).name || 'Chat!',
+    title: (navigation.state.params || {}).name || 'Chat',
   });
+
+  state = {
+    messages: [],
+  };
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello :)',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
 
   render() {
     return (
-      <View style={styles.textStyle}>
-        <Text>Hi</Text>
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     );
   }
 }
