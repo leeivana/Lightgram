a77165fb2c5d053c9a880f775d66c82bd124a0eb
leeivana <lee.ivana@hotmail.com>
2018-12-18 11:39:06 -0500

    Deleted file

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 import MainTabNavigator from './MainTabNavigator';
-import ChatListScreen from '../screens/ChatListScreen';
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
-  Chat: ChatListScreen,
 });
