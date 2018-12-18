c092c614000a5728814fa4fae1384e5fb7c7bba7
leeivana <lee.ivana@hotmail.com>
2018-12-18 13:15:17 -0500

    Created tabnavigation file

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TabNavigator } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+
+import ChatListScreen from '../screens/ChatListScreen';
+import ChatScreen from '../screens/ChatScreen';
