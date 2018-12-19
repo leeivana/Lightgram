96043974fa8d33f0d78afcea6e2e39ef827c418e
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-12-19 17:26:46 -0500

    added logic for sign up and navigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, Text, StatusBar, StyleSheet, View } from 'react-native';
 // import { AppLoading, Asset, Font, Icon } from 'expo';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
@@ -13,6 +13,20 @@ export default class App extends React.Component {
   state = {
     user: null,
   };
+
+  async componentDidMount() {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      console.log('user: ', user);
+      if (user) {
+        return <AppNavigator />;
+      }
+      return <LoginScreen setUser={u => this.setState({ user: u })} />;
+    } catch (err) {
+      console.log('error: ', err);
+      return <LoginScreen setUser={u => this.setState({ user: u })} />;
+    }
+  }
 
   render() {
     const { user } = this.state;
@@ -34,5 +48,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  welcome: {
+    fontSize: 28,
   },
 });
