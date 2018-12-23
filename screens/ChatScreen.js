6e65605d1f7799206325ff1f1d965ff9cf0619a7
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2018-12-23 14:30:54 -0500

    cleaned up code

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { Button } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import {
-  GiftedChat,
-  Actions,
-  Bubble,
-  SystemMessage,
-} from 'react-native-gifted-chat';
+import { GiftedChat } from 'react-native-gifted-chat';
 import { API, graphqlOperation } from 'aws-amplify';
 
 // mutation to create messages given a certain chatid
-// query for all messages given a certain chatid 
-// subscription for new messages in the chat 
+// query for all messages given a certain chatid
+// subscription for new messages in the chat
 
-//replace id with id of specific conversation 
+// replace id with id of specific conversation
 const ListMessages = `
 query list{
   getConvo(id: "a195a3ad-d953-4fb6-a26b-19ebb94eeaf9"){
@@ -32,8 +27,6 @@ query list{
 }
 `;
 
-const CreateMessage = ``
-
 class ChatScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: (navigation.state.params || {}).first_name || 'Chat',
@@ -51,48 +44,28 @@ class ChatScreen extends Component {
     messages: [],
   };
 
-  //Updates messages 
+  // Updates messages
   async componentDidMount() {
-    const messageData = await API.graphql(graphqlOperation(ListMessages)); 
+    const messageData = await API.graphql(graphqlOperation(ListMessages));
     const allMessages = messageData.data.getConvo.messages.items;
     console.log('all messages', allMessages);
     allMessages.forEach(el => {
-      let newMessage = {_id: el.id, 
+      const { messages } = this.state;
+
+      const newMessage = {
+        _id: el.id,
         text: el.content,
         createdAt: el.createdAt,
         user: {
           _id: el.authorId,
-          name: el.author['given_name'],
-        }
-      }; 
-      this.setState(currentState => ({
-        messages: [
-          newMessage, ...this.state.messages
-        ]
-      }))
-    })
-    // console.log('State', this.state.messages);
+          name: el.author.given_name,
+        },
+      };
+      this.setState({
+        messages: [newMessage, ...messages],
+      });
+    });
   }
-
-  //SETSTATE NOTES 
-  // Should be an array of objects that follow this format:
-  // id for message
-  // text - content 
-  // new date 
-  //user info - user id , user name, avatar (OBJECT)
-
-  //     messages: [
-  //       {
-  //         _id: 1,
-  //         text: 'Hello :)',
-  //         createdAt: new Date(),
-  //         user: {
-  //           _id: 2,
-  //           name: 'React Native',
-  //           avatar: 'https://placeimg.com/140/140/any',
-  //         },
-  //       },
-  //     ],
 
   onSend(messages = []) {
     this.setState(previousState => ({
@@ -101,9 +74,10 @@ class ChatScreen extends Component {
   }
 
   render() {
+    const { messages } = this.state;
     return (
       <GiftedChat
-        messages={this.state.messages}
+        messages={messages}
         onSend={messages => this.onSend(messages)}
         user={{
           _id: 1,
