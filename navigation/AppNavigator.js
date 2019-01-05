72ef6c3399e5afb15203a19db11350640bdbdddb
Serhii Panchyshyn <panchyshyn.serhii@gmail.com>
2019-01-05 18:58:16 +0200

    cleaned up code

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,10 +2,9 @@ import { createSwitchNavigator } from 'react-navigation';
 
 import MainAppNavigator from './MainAppNavigator';
 import AuthScreenNavigation from './AuthScreenNavigation';
+// import InitializingScreen from './InitializingScreen';
 
 export default createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   // Initializing: { screen: Initializing },
   Auth: { screen: AuthScreenNavigation },
   Main: { screen: MainAppNavigator },
