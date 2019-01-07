d76dfe26e0e047af65c1daf26508f2ae704d7dba
leeivana <lee.ivana@hotmail.com>
2019-01-06 19:54:52 -0500

    Implemented promise.all to retrieve all messages with given name

--- screens/MainAppScreen/ChatScreen.js
+++ screens/MainAppScreen/ChatScreen.js
@@ -37,39 +37,39 @@ class ChatScreen extends Component {
 
   // Updates messages
   async componentDidMount() {
+    const newMessageArray = [];
+    const { messages } = this.state;
     const messageData = await API.graphql(
       graphqlOperation(getConvo, { id: '28ff8871-d4d7-4620-8294-34c3ffa0b8ad' })
     );
     const allMessages = messageData.data.getConvo.messages.items;
-    allMessages.forEach(el => {
-      const { messages } = this.state;
-      const { id, content, createdAt, authorId } = el;
-      // const { given_name } = el.author;
-      const newMessage = {
-        _id: id,
-        text: content,
-        createdAt,
-        user: {
-          _id: authorId,
-          name: '',
-        },
-      };
-
-      (async () => {
-        const payload = await API.graphql(
-          graphqlOperation(basicUserQuery, { id: authorId })
-        );
-        console.log('payload', payload);
-        const givenName = payload.data.getUser.given_name;
-        newMessage.user.name = givenName;
-        console.log('obj', newMessage);
-      })().then(
-        this.setState({
-          messages: [newMessage, ...messages],
+    console.log('all messages', allMessages);
+    try {
+      await Promise.all(
+        allMessages.map(async el => {
+          el.isSent = true;
+          const { id, content, createdAt, authorId } = el;
+          const newMessage = {
+            _id: id,
+            text: content,
+            createdAt,
+            user: {
+              _id: authorId,
+              name: '',
+            },
+          };
+          const payload = await API.graphql(
+            graphqlOperation(basicUserQuery, { id: authorId })
+          );
+          const givenName = payload.data.getUser.given_name;
+          newMessage.user.name = givenName;
+          newMessageArray.push(newMessage);
         })
       );
-      console.log('state', this.state.messages);
-    });
+      this.setState({ messages: newMessageArray, ...messages });
+    } catch (e) {
+      console.error(e);
+    }
     API.graphql(
       graphqlOperation(onCreateMessage, {
         messageConversationId: 'a195a3ad-d953-4fb6-a26b-19ebb94eeaf9',
